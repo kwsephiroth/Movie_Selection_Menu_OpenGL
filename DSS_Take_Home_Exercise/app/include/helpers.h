a96eb12995464bc88aa537318c199df4a83af85d
Kenneth Walker <kwalkerdev@gmail.com>
2021-01-20 14:59:07 -0800

    Added header and updated gitignore file.

---
+++ DSS_Take_Home_Exercise/app/include/helpers.h
@@ -0,0 +1,12 @@
+#ifndef HELPERS_H_
+#define HELPERS_H_
+
+#include <rapidjson/document.h>
+#include <optional>
+
+/*std::optional<rapidjson::Document> get_json_doc_from_file(const char* filename)
+{
+
+}*/
+
+#endif // HELPERS
