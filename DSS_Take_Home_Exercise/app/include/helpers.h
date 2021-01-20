51ceafcb6730090676b6d340f10265337732399b
Kenneth Walker <kwalkerdev@gmail.com>
2021-01-20 15:03:45 -0800

    Changed C++ standard to C++17

--- DSS_Take_Home_Exercise/app/include/helpers.h
+++ DSS_Take_Home_Exercise/app/include/helpers.h
@@ -4,9 +4,9 @@
 #include <rapidjson/document.h>
 #include <optional>
 
-/*std::optional<rapidjson::Document> get_json_doc_from_file(const char* filename)
+std::optional<rapidjson::Document> get_json_doc_from_file(const char* filename)
 {
-
-}*/
+	std::nullopt;
+}
 
 #endif // HELPERS
