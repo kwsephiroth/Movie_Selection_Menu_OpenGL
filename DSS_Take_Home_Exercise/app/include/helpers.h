9c798cc56aaa8807676111cf1f3a7c813a0962b1
Kenneth Walker <kwalkerdev@gmail.com>
2021-01-20 19:50:51 -0800

    Pulled in curl library for file downloads.

--- DSS_Take_Home_Exercise/app/include/helpers.h
+++ DSS_Take_Home_Exercise/app/include/helpers.h
@@ -2,11 +2,34 @@
 #define HELPERS_H_
 
 #include <rapidjson/document.h>
+#include <rapidjson/filereadstream.h>
 #include <optional>
 
-std::optional<rapidjson::Document> get_json_doc_from_file(const char* filename)
+namespace DSS
 {
-	std::nullopt;
-}
+	std::optional<rapidjson::Document> get_json_doc_from_file(const char* filename)
+	{
+		FILE* fp = fopen(filename, "rb"); // non-Windows use "r"
 
+		if (!fp)
+		{
+			return std::nullopt;
+		}
+
+		char readBuffer[65536];
+		rapidjson::FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+
+		rapidjson::Document d;
+		d.ParseStream(is);
+
+		fclose(fp);
+
+		if (!d.HasParseError())
+		{
+			return d;
+		}
+
+		return std::nullopt;
+	}
+}
 #endif // HELPERS
--- DSS_Take_Home_Exercise/app/src/main.cpp
+++ DSS_Take_Home_Exercise/app/src/main.cpp
@@ -1,50 +1,144 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <curl/curl.h>
+#include "helpers.h"
 
 int main(void)
 {
-    GLFWwindow* window;
+    //auto rj_doc_opt = DSS::get_json_doc_from_file("app/res/home.json");
+    //if (!rj_doc_opt.has_value())
+    //{
+    //    std::cout << "Failed to create json document from file 'res/home.json'" << std::endl;
+    //}
+    //auto rj_doc = std::move(rj_doc_opt.value());
+    //for (auto itr = rj_doc.MemberBegin(); itr != rj_doc.MemberEnd(); ++itr)
+    //{
+    //    std::cout << itr->name.GetString() << std::endl;
+    //}
+    //std::cin.get();
+    //GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+    ///* Initialize the library */
+    //if (!glfwInit())
+    //    return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+    ///* Create a windowed mode window and its OpenGL context */
+    //window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    //if (!window)
+    //{
+    //    glfwTerminate();
+    //    return -1;
+    //}
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    ///* Make the window's context current */
+    //glfwMakeContextCurrent(window);
 
-    /* Initialize glew*/
-    GLenum err = glewInit();
-    if (GLEW_OK != err)
-    {
-        /* Problem: glewInit failed, something is seriously wrong. */
-        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-    }
-    fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+    ///* Initialize glew*/
+    //GLenum err = glewInit();
+    //if (GLEW_OK != err)
+    //{
+    //    /* Problem: glewInit failed, something is seriously wrong. */
+    //    fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+    //}
+    //fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
 
-    std::cout << glGetString(GL_VERSION) << std::endl;
+    //std::cout << glGetString(GL_VERSION) << std::endl;
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+    ///* Loop until the user closes the window */
+    //while (!glfwWindowShouldClose(window))
+    //{
+    //    /* Render here */
+    //    glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+    //    /* Swap front and back buffers */
+    //    glfwSwapBuffers(window);
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+    //    /* Poll for and process events */
+    //    glfwPollEvents();
+    //}
 
-    glfwTerminate();
+    //glfwTerminate();
     return 0;
 }
+
+/***************************************************************************
+ *                                  _   _ ____  _
+ *  Project                     ___| | | |  _ \| |
+ *                             / __| | | | |_) | |
+ *                            | (__| |_| |  _ <| |___
+ *                             \___|\___/|_| \_\_____|
+ *
+ * Copyright (C) 1998 - 2018, Daniel Stenberg, <daniel@haxx.se>, et al.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution. The terms
+ * are also available at https://curl.haxx.se/docs/copyright.html.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the COPYING file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/
+ /* <DESC>
+  * Download a given URL into a local file named page.out.
+  * </DESC>
+  */
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <curl/curl.h>
+//
+//static size_t write_data(void* ptr, size_t size, size_t nmemb, void* stream)
+//{
+//    size_t written = fwrite(ptr, size, nmemb, (FILE*)stream);
+//    return written;
+//}
+//
+//int main(int argc, char* argv[])
+//{
+//    CURL* curl_handle;
+//    static const char* pagefilename = "page.out";
+//    FILE* pagefile;
+//
+//    curl_global_init(CURL_GLOBAL_ALL);
+//
+//    /* init the curl session */
+//    curl_handle = curl_easy_init();
+//
+//    /* set URL to get here */
+//    curl_easy_setopt(curl_handle, CURLOPT_URL, "https://cd-static.bamgrid.com/dp-117731241344/home.json");//argv[1]);
+//
+//    /* Switch on full protocol/debug output while testing */
+//    curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
+//
+//    /* disable progress meter, set to 0L to enable and disable debug output */
+//    curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
+//
+//    /* send all data to this function  */
+//    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
+//
+//    /* open the file */
+//    pagefile = fopen(pagefilename, "wb");
+//    if (pagefile) {
+//
+//        /* write the page body to this file handle */
+//        curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, pagefile);
+//
+//        /* get it! */
+//        curl_easy_perform(curl_handle);
+//
+//        /* close the header file */
+//        fclose(pagefile);
+//    }
+//
+//    /* cleanup curl stuff */
+//    curl_easy_cleanup(curl_handle);
+//
+//    curl_global_cleanup();
+//
+//    return 0;
+//}
